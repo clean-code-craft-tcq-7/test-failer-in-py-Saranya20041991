4162e0d4a8933291bd1c56f66209680d7e818a66
Saranya <saranyaacbe@yahoo.com>
2025-08-05 12:04:57 +0530

    refactor based on assert

--- tshirts.py
+++ tshirts.py
@@ -1,14 +1,21 @@
 
 def size(cms):
-    if cms < 38:
+    if cms > 35 and cms < 38:
         return 'S'
-    elif cms > 38 and cms < 42:
+    elif cms > 37 and cms < 42:
         return 'M'
-    else:
+    elif cms > 41 and cms < 45:
         return 'L'
-
-
+   
 assert(size(37) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
+assert(size(37) == 'S')
+assert(size(38) == 'S')
+assert(size(36) == 'M')
+assert(size(38) == 'M')
+assert(size(42) == 'M')
+assert(size(40) == 'L')
+assert(size(42) == 'L')
+ 
 print("All is well (maybe!)")
